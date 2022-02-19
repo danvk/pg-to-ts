3f743e2957dd343c0cd8b2bd28a5b6ae475f2156
Dan Vanderkam <danvdk@gmail.com>
2022-02-19 18:57:45 -0500

    Switch to snapshots

--- test/integration/schema-generation-test.ts
+++ test/integration/schema-generation-test.ts
@@ -1,5 +1,5 @@
 import {PostgresDatabase} from '../../src/schemaPostgres';
-import {writeTsFile, compare, loadSchema} from '../testUtility';
+import {getGeneratedTs, loadSchema} from '../testUtility';
 
 export const schemaGenerationTest = () => {
   describe('postgres', () => {
@@ -25,29 +25,20 @@ export const schemaGenerationTest = () => {
 
     it('Basic generation', async () => {
       const inputSQLFile = 'test/fixture/postgres/osm.sql';
-      const outputFile = './test/actual/postgres/osm.ts';
-      const expectedFile = './test/expected/postgres/osm.ts';
       const config = './test/fixture/postgres/osm.json';
-      await writeTsFile(inputSQLFile, config, outputFile, db);
-      expect(await compare(expectedFile, outputFile)).toBeTruthy();
+      expect(await getGeneratedTs(inputSQLFile, config, db)).toMatchSnapshot();
     });
 
     it('Camelcase generation', async () => {
       const inputSQLFile = 'test/fixture/postgres/osm.sql';
-      const outputFile = './test/actual/postgres/osm-camelcase.ts';
-      const expectedFile = './test/expected/postgres/osm-camelcase.ts';
       const config = './test/fixture/postgres/osm-camelcase.json';
-      await writeTsFile(inputSQLFile, config, outputFile, db);
-      expect(await compare(expectedFile, outputFile)).toBeTruthy();
+      expect(await getGeneratedTs(inputSQLFile, config, db)).toMatchSnapshot();
     });
 
     it('pg-to-sql features', async () => {
       const inputSQLFile = 'test/fixture/postgres/pg-to-ts.sql';
-      const outputFile = './test/actual/postgres/pg-to-ts.ts';
-      const expectedFile = './test/expected/postgres/pg-to-ts.ts';
       const config = './test/fixture/postgres/pg-to-ts.json';
-      await writeTsFile(inputSQLFile, config, outputFile, db);
-      expect(await compare(expectedFile, outputFile)).toBeTruthy();
+      expect(await getGeneratedTs(inputSQLFile, config, db)).toMatchSnapshot();
     });
   });
 };
--- test/testUtility.ts
+++ test/testUtility.ts
@@ -1,45 +1,6 @@
 import * as fs from 'fs/promises';
 import {typescriptOfSchema} from '../src/index';
-import {diffLines} from 'diff';
 import {PostgresDatabase} from '../src/schemaPostgres';
-
-interface IDiffResult {
-  value: string;
-  count?: number;
-  added?: boolean;
-  removed?: boolean;
-}
-
-export async function compare(
-  goldStandardFile: string,
-  outputFile: string,
-): Promise<boolean> {
-  const gold = await fs.readFile(goldStandardFile, {encoding: 'utf8'});
-  const actual = await fs.readFile(outputFile, {encoding: 'utf8'});
-
-  const diffs = diffLines(gold, actual, {
-    ignoreWhitespace: true,
-    newlineIsToken: true,
-    ignoreCase: false,
-  });
-
-  const addOrRemovedLines = diffs.filter(
-    (d: IDiffResult) => d.added || d.removed,
-  );
-
-  if (addOrRemovedLines.length > 0) {
-    console.error(
-      `Generated type definition different to the standard ${goldStandardFile}`,
-    );
-    addOrRemovedLines.forEach((d: IDiffResult, i: number) => {
-      const t = d.added ? '+' : d.removed ? '-' : 'x';
-      console.error(`  [${i}] ${t} ${d.value}`);
-    });
-    return false;
-  } else {
-    return true;
-  }
-}
 
 export async function loadSchema(db: PostgresDatabase, file: string) {
   const query = await fs.readFile(file, {
@@ -48,21 +9,13 @@ export async function loadSchema(db: PostgresDatabase, file: string) {
   return await db.query(query);
 }
 
-export async function writeTsFile(
+export async function getGeneratedTs(
   inputSQLFile: string,
   inputConfigFile: string,
-  outputFile: string,
   db: PostgresDatabase,
 ) {
   await loadSchema(db, inputSQLFile);
   const config = JSON.parse(await fs.readFile(inputConfigFile, 'utf8'));
   const {tables, schema, ...options} = config;
-  const formattedOutput = await typescriptOfSchema(
-    db,
-    tables,
-    [],
-    schema,
-    options,
-  );
-  await fs.writeFile(outputFile, formattedOutput);
+  return typescriptOfSchema(db, tables, [], schema, options);
 }
