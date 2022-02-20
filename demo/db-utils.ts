177d448b9011d8bcfe790d9554eaad9a81c8f888
Dan Vanderkam <danvdk@gmail.com>
2022-02-20 18:57:13 -0500

    Select specific columns

--- demo/db-utils.ts
+++ demo/db-utils.ts
@@ -12,6 +12,7 @@ class TypedSQL<SchemaT> {
     const fn = () => {
       return [tableName] as any;
     };
+    fn.columns = (cols: any[]) => fn;
     return fn;
   }
 }
@@ -20,8 +21,23 @@ type LooseKey<T, K> = T[K & keyof T];
 type LooseKey2<T, K1, K2> = LooseKey<LooseKey<T, K1>, K2>;
 type LooseKey3<T, K1, K2, K3> = LooseKey<LooseKey2<T, K1, K2>, K3>;
 
-interface Select<SchemaT, Table extends Omit<keyof SchemaT, '$schema'>> {
-  (): LooseKey3<SchemaT, '$schema', Table, 'select'>[];
+type LoosePick<T, K> = SimplifyType<Pick<T, K & keyof T>>;
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type SimplifyType<T> = T extends Function ? T : {[K in keyof T]: T[K]};
+
+interface Select<
+  SchemaT,
+  Table extends Omit<keyof SchemaT, '$schema'>,
+  Cols = null,
+> {
+  (): [Cols] extends [null]
+    ? LooseKey3<SchemaT, '$schema', Table, 'select'>[]
+    : LoosePick<LooseKey3<SchemaT, '$schema', Table, 'select'>, Cols>[];
+
+  columns<NewCols extends keyof LooseKey3<SchemaT, '$schema', Table, 'select'>>(
+    cols: NewCols[],
+  ): Select<SchemaT, Table, NewCols>;
 }
 
 const typedDb = new TypedSQL(tables);
@@ -29,3 +45,22 @@ const typedDb = new TypedSQL(tables);
 const selectComment = typedDb.select('comment');
 const comments = selectComment();
 // type is Comment[]!
+
+const selectCommentCols = selectComment.columns([
+  'id',
+  'author_id',
+  'content_md',
+]);
+const narrowedComments = selectCommentCols();
+// type is {
+//     id: string;
+//     author_id: string;
+//     content_md: string;
+// }[]
+// This is a good example of a place where preventing distribution is key ([Cols] extends [null])
+// and where SimplifyType is key (otherwise LoosePick shows up).
+// See commit
+
+// Notes on type display:
+// This is gross:
+// const comments: LoosePick<Comment, keyof Comment>[]
