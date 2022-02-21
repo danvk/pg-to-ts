dcb3333c82b5c9a1cfa13a574d0c4d139aedee3e
Dan Vanderkam <danvdk@gmail.com>
2022-02-21 13:39:11 -0500

    Notes; orderBy

---
+++ demo/README.md
@@ -0,0 +1,29 @@
+# DB Utils
+
+## Todo
+
+- [ ] Insert
+- [ ] Update
+- [ ] Support sets in `select`
+- [ ] `selectByPrimaryKey`
+- [ ] Delete
+- [ ] Set up type testing, ideally via string matching
+- [ ] Implement runtime
+
+## Other related tools
+
+- Sqlpture
+- zapatos
+- PgTyped
+- pg-promise
+- kysely (query builder) <https://github.com/koskimas/kysely>
+- knex (query builder) <https://knexjs.org/>
+
+Rants about ORMs / query builders:
+- <https://hackernoon.com/migrating-from-query-builders-and-orms-in-javascript-or-typescript-gc113uh0>
+- https://www.reddit.com/r/typescript/comments/jcw28f/typeorm_sucks_something_i_wanted_to_talk_about/
+- https://www.prisma.io/dataguide/database-tools/evaluating-type-safety-in-the-top-8-typescript-orms
+- <https://www.jakso.me/blog/objection-to-orm-hatred> (Pro ORM)
+- <https://github.com/mmkal/slonik-tools/tree/master/packages/typegen#sloniktypegen>
+- <https://gajus.medium.com/stop-using-knex-js-and-earn-30-bf410349856c>
+
--- demo/db-utils.ts
+++ demo/db-utils.ts
@@ -26,16 +26,29 @@ type LoosePick<T, K> = SimplifyType<Pick<T, K & keyof T>>;
 // eslint-disable-next-line @typescript-eslint/ban-types
 type SimplifyType<T> = T extends Function ? T : {[K in keyof T]: T[K]};
 
-interface Select<TableT, Cols = null> {
-  (): [Cols] extends [null] ? TableT[] : LoosePick<TableT, Cols>[];
+type Order<Cols> = [column: Cols, order: 'ASC' | 'DESC'];
+type OrderBy<Cols> = Order<Cols>[];
+
+interface Select<TableT, Cols = null, WhereCols = never> {
+  // TODO: only provide one of these as appropriate
+  (
+    ...args: [WhereCols] extends [never]
+      ? []
+      : [where: LoosePick<TableT, WhereCols>]
+  ): [Cols] extends [null] ? TableT[] : LoosePick<TableT, Cols>[];
 
   columns<NewCols extends keyof TableT>(
     cols: NewCols[],
-  ): Select<TableT, NewCols>;
+  ): Select<TableT, NewCols, WhereCols>;
 
-  // TODO:
-  // - where()
-  // - whereDynamic()
+  where<WhereCols extends keyof TableT>(
+    cols: WhereCols[],
+  ): Select<TableT, Cols, WhereCols>;
+
+  orderBy(order: OrderBy<keyof TableT>): this;
+
+  // TODO: orderBy
+  // TODO: allow matching a set
 }
 
 const typedDb = new TypedSQL(tables);
@@ -43,6 +56,8 @@ const typedDb = new TypedSQL(tables);
 const selectComment = typedDb.select('comment');
 const comments = selectComment();
 // type is Comment[]!
+// @ts-expect-error Cannot pass argument without where()
+selectComment({});
 
 const selectCommentCols = selectComment.columns([
   'id',
@@ -58,6 +73,24 @@ const narrowedComments = selectCommentCols();
 // This is a good example of a place where preventing distribution is key ([Cols] extends [null])
 // and where SimplifyType is key (otherwise LoosePick shows up).
 // See commit 177d448
+
+const selectCommentsById = selectComment.where(['id']);
+const commentsById = selectCommentsById({id: '123'});
+// type is Comment[]
+// @ts-expect-error Must pass ID
+selectCommentsById();
+// @ts-expect-error Cannot pass other columns (though only because of EPC)
+selectCommentsById({id: '123', author_id: 'abc'});
+
+const orderedSelectAll = selectComment.orderBy([['created_at', 'DESC']]);
+const orderedComments = orderedSelectAll();
+// type is Comment[]
+
+// @ts-expect-error desc should be capitalized
+selectComment.orderBy([['created_at', 'desc']]);
+
+// @ts-expect-error needs to be array of tuples, not just one tuple
+selectComment.orderBy(['created_at', 'desc']);
 
 // Notes on type display:
 // This is gross:
